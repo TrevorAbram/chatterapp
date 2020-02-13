86e0be650d9230a6f5c7eff3909e5abfbc2c00d3
Trevor Abram <trevor@fideagency.com>
2020-02-13 13:19:12 -0600

    User can see recent mess when channel joined

--- lib/chatter_app_web/channels/chat_room_channel.ex
+++ lib/chatter_app_web/channels/chat_room_channel.ex
@@ -30,6 +30,25 @@ defmodule ChatterAppWeb.ChatRoomChannel do
     {:noreply, socket}
   end
 
+  def join('chat_room:lobby', payload, socket do
+    if authorized?(payload) do
+      send(self(), :after_join)
+      {:ok, socket}
+
+    else
+      {:error, %{'reason: unauthorized!'}}
+    end
+
+  end
+
+  def handle_info(:after_join, socket) do
+    {:no_reply, socket}
+  end
+
+
+
+
+
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true

6d033cf49457bc66ad7763d74fa0c8e16f69385a
Trevor Abram <trevor@fideagency.com>
2020-02-27 20:45:30 -0600

    Remove duplicate join/3 argument in chat_room_channel

--- lib/chatter_app_web/channels/chat_room_channel.ex
+++ lib/chatter_app_web/channels/chat_room_channel.ex
@@ -1,14 +1,6 @@
 defmodule ChatterAppWeb.ChatRoomChannel do
   use ChatterAppWeb, :channel
 
-  def join("chat_room:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
-  end
-
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   def handle_in("ping", payload, socket) do
